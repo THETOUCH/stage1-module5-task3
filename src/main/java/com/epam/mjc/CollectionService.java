5517e3f6144850aed6ec39dcf26888dd028983be
THETOUCH <super.ilgiznex@gmail.com>
2023-11-07 02:01:24 +0600

    123

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,38 @@
 package com.epam.mjc;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().map(String::toUpperCase).collect(Collectors.toList());
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().max(Integer::compareTo);
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+//        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().flatMap(Collection::stream).min(Integer::compareTo);
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (list.stream().reduce(Integer::sum).isPresent()) {
+            return list.stream().reduce(Integer::sum).get();
+        } else {
+            throw new RuntimeException("This list has no elements");
+        }
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
